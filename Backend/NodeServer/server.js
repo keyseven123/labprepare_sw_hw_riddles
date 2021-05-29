d40b1e1be0b0f8e6ca95727bba942e886adf0d4b
Lukas <l.george@tu-berlin.de>
2021-05-29 14:55:30 +0200

    Time to test GPIO access on the PI

--- Backend/NodeServer/server.js
+++ Backend/NodeServer/server.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()   
 const port = 2999       //subject to change
 
+const gpio = require('onoff').Gpio
+const testLED = new gpio(4, 'out')
+
 app.get('/', (req, res) => {
+    if (testLED.readSync() === 0) {
+        testLED.writeSync(1)
+    } else {
+        testLED.writeSync(0)
+    }
     res.send('Hello World')
 })
 
