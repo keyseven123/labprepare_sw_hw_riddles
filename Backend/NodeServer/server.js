b28eef32768439d88f1b3447fde3dbf72639357d
Lukas <l.george@tu-berlin.de>
2021-06-07 12:45:52 +0200

    Nodeserver: Can now create riddles and add hints
    Create a riddle and add it to the list by using a post request at
    serverurl/createRiddle and add a hint with a post request to
    serverurl/addHint/:riddleId.
    Example riddle: curl -d '{"name":"Test Riddle","description":"This Riddle serves as a Test","difficulty":4}' -H 'content-type:application/json' "localhost:2999/createRiddle"
    Example hint: curl -d ' {"id":0,"name":"Test Hint","description":"This is a useless Hint as a Test"}' -H 'content-type:application/json' "localhost:2999/addHint/0"
    I can add more fields to the class later when we require them
    The list of riddles is returned when requesting GET serverurl/ as json and
    includes all riddles each with its own list of hints

--- Backend/NodeServer/server.js
+++ Backend/NodeServer/server.js
@@ -1,18 +1,116 @@
 const express = require('express')
 const app = express()   
 const port = 2999       //subject to change
+app.use(express.json());
+var riddles = []
 
-const gpio = require('onoff').Gpio
-const testLED = new gpio(4, 'out')
+class Hint {
+    constructor(id, name, description) {
+        process.stdout.write(`Creating new Hint: `)
+        if (id == null || !id instanceof Number) {
+            process.stdout.write(`FAIL - invalid id ${id}\n`)
+            return null
+        }
+        if (name == null || !name instanceof String) {
+            process.stdout.write(`FAIL - invalid name ${name}\n`)
+            return null
+        }
+        if (description == null || !description instanceof String) {
+            process.stdout.write(`FAIL - invalid description ${description}\n`)
+            return null
+        }
+        this.id  = id
+        this.name = name
+        this.description = description
+        process.stdout.write("SUCCESS\n")
+    }
+
+}
+
+class Riddle {
+    constructor(id, name, description, difficulty) {
+        process.stdout.write(`Creating new Riddle: `)
+        if (id == null || !id instanceof Number) {
+            process.stdout.write(`FAIL - invalid id ${id}\n`)
+            return null
+        }
+        if (name == null || !name instanceof String) {
+            process.stdout.write(`FAIL - invalid name ${name}\n`)
+            return null
+        }
+        if (description == null || !description instanceof String) {
+            process.stdout.write(`FAIL - invalid description ${description}\n`)
+            return null
+        }
+        if (difficulty == null || !difficulty instanceof Number || difficulty < 0 || difficulty > 10) {
+            process.stdout.write(`FAIL - invalid difficulty ${difficulty}\n`)
+            return null
+        }
+        this.id  = id
+        this.name = name
+        this.description = description
+        this.difficulty = difficulty
+        this.hints = []
+        riddles.push(this)
+        process.stdout.write("SUCCESS\n")
+    }
+
+    addHint(hint) {
+        process.stdout.write(`Adding hint to riddle ${this.id}: `)
+        if (hint == null || !hint instanceof Hint) {
+            process.stdout.write("FAIL - invalid hint format\n")
+            return
+        }
+        this.hints.push(hint)
+        process.stdout.write("SUCCESS\n")
+    }
+    
+}
+
+const raspi = false
+if (raspi) {
+    const gpio = require('onoff').Gpio
+    const testLED = new gpio(4, 'out')
+}
 
 app.get('/', (req, res) => {
-    if (testLED.readSync() === 0) {
-        testLED.writeSync(1)
-    } else {
-        testLED.writeSync(0)
+    if (raspi) {
+        if (testLED.readSync() === 0) {
+            testLED.writeSync(1)
+        } else {
+            testLED.writeSync(0)
+        }
     }
-    res.send('Hello World')
+
+    
+    res.json(riddles)
 })
+
+// curl -d '{"name":"Test Riddle","description":"This Riddle serves as a Test","difficulty":4}' -H 'content-type:application/json' "localhost:2999/createRiddle"
+app.post('/createRiddle', (req, res) => {
+    if (req.body.name == null || req.body.description == null || req.body.difficulty == null) {
+        console.log("Wrong parameters for creating a riddle")
+        return res.status(400).send()
+    }
+    let myRiddle = new Riddle(riddles.length, req.body.name, req.body.description, req.body.difficulty)
+    return res.status(200).send()
+})
+
+// curl -d ' {"id":0,"name":"Test Hint","description":"This is a useless Hint as a Test"}' -H 'content-type:application/json' "localhost:2999/addHint/0"
+app.post('/addHint/:riddleId', (req, res) => {
+    if (req.body.name == null || req.body.description == null) {
+        console.log("Wrong parameters for creating a hint")
+        return res.status(400).send()
+    }
+    let myRiddle = riddles.find(x => x.id == req.params.riddleId)
+    if (myRiddle === undefined) {
+        console.log(`Unable to add a hint to riddle ${req.params.riddleId} - it does not exist`)
+        return res.status(404).send()
+    }
+    myRiddle.addHint(new Hint(myRiddle.hints.length, req.body.name, req.body.description))
+    return res.status(200).send()
+})
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
