48a6eeeba249d0b7b85c7361afe863745241c4c1
oliver99str <57124316+oliver99str@users.noreply.github.com>
2021-10-09 08:25:47 +0200

    NEW: Fetch quiz info every 1 second
    Meta info, like completion state is fetched from the server every second. Brings dynamic changes into frontend if the server responds so. If you did something correct on the backend side, the frontend will update the information every second to inform the user. 
    
    e.g: Did I complete the task after setting the right circuit on the arduino ?

--- Frontend2/src/App.js
+++ Frontend2/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router} from 'react-router-dom';
+import React, { useEffect } from 'react';
 import Dashboard from './containers/Dashboard'
-import { useDispatch } from "react-redux";
+import { useDispatch} from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "./state/index";
 
@@ -8,7 +9,13 @@ function App() {
   const dispatch = useDispatch();
   const {initBackendData} = bindActionCreators(actionCreators, dispatch);
 
-  initBackendData();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      initBackendData();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <Router>
       <Dashboard/>
